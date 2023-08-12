81c5acf0c96f1469e3915b86ba84d6bf3202ed8e
atsushimemet <noap3b69n@gmail.com>
2023-08-12 14:47:35 +0900

    add: aws_route_tableリソースprivate

--- module/network/main.tf
+++ module/network/main.tf
@@ -84,3 +84,14 @@ resource "aws_subnet" "private" {
     Name = "${var.name}-private-${each.key}"
   }
 }
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.default.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.default.id
+  }
+  tags = {
+    Name = "${var.name}-private-rtb"
+  }
+}
