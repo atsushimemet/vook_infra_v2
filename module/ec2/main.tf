c07658fb7d30e9e368a6ee00579be916db1564ef
atsushimemet <noap3b69n@gmail.com>
2023-08-12 15:10:33 +0900

    add: IAM Role

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -58,4 +58,37 @@ resource "aws_security_group_rule" "ec2_ingress_https" {
   ]
   security_group_id = aws_security_group.ec2.id
 }
- 
+
+#--------------------------------------------------------------
+# IAM Role
+#--------------------------------------------------------------
+
+data "aws_iam_policy_document" "ec2" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy" "systems-manager" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role" "ec2" {
+  name               = "${var.app_name}-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2.json
+}
+
+resource "aws_iam_role_policy_attachment" "ec2" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = data.aws_iam_policy.systems-manager.arn
+}
+
+resource "aws_iam_instance_profile" "systems-manager" {
+  name = "${var.app_name}-ec2-instance-profile"
+  role = aws_iam_role.ec2.name
+}
