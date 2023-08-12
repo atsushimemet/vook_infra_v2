2f6cfc2def5997c77c78177d8af37702d04789c5
atsushimemet <noap3b69n@gmail.com>
2023-08-12 15:09:06 +0900

    add: security group

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -4,3 +4,58 @@
 data "aws_ami" "amzn2" {
   name = "ami-06fdbb60c8e83aa5e"
 }
+
+#--------------------------------------------------------------
+# Security group
+#--------------------------------------------------------------
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "ec2" {
+  name = "${var.app_name}-ec2-sg"
+
+  description = "EC2 service security group for ${var.app_name}"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_security_group_rule" "ec2_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2.id
+}
+
+resource "aws_security_group_rule" "ec2_ingress_ssh" {
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  security_group_id = aws_security_group.ec2.id
+}
+
+resource "aws_security_group_rule" "ec2_ingress_http" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  security_group_id = aws_security_group.ec2.id
+}
+ 
+resource "aws_security_group_rule" "ec2_ingress_https" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  security_group_id = aws_security_group.ec2.id
+}
+ 
