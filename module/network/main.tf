8f92f25d544849b97183c5ac799ae32640991708
atsushimemet <noap3b69n@gmail.com>
2023-08-12 14:42:57 +0900

    add: aws_route_table_associationリソース

--- module/network/main.tf
+++ module/network/main.tf
@@ -54,3 +54,10 @@ resource "aws_route_table" "public" {
     Name = "${var.name}-public-rtb"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
